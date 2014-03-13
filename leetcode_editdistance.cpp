b765541ebd94c3a14c7fc7b4dd17b07fba08e13f
yular <1062969706@qq.com>
2014-03-13 13:14:47 -0700

    leetcode editdistance

---
+++ leetcode_editdistance.cpp
@@ -0,0 +1,30 @@
+/*
+* Algorithm: DP
+* Time complexity: O(n^2)
+* Memory complexity: O(n^2)
+*
+*/
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.size(), len2 = word2.size();
+        vector<vector<int> > dp(len1 + 1, vector<int>(len2 + 1, 0));
+        for(int i = 0; i <= len1; i ++)
+            dp[i][0] = i;
+        for(int i = 0; i <= len2; i ++)
+            dp[0][i] = i;
+        for(int i = 0; i < len1; i ++){
+            for(int j = 0; j < len2; j ++){
+                dp[i + 1][j + 1] = 1<<29;
+                if(word1[i] == word2[j]){
+                    dp[i + 1][j + 1] = dp[i][j];
+                }else{
+                    dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i + 1][j] + 1);
+                    dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j + 1] + 1);
+                    dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j] + 1);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+};
