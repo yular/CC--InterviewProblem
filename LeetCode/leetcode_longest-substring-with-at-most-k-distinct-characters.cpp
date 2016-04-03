d79e19885a39c09ed517ed04ed18551bf9b103cc
Baichuan Yang <1062969706@qq.com>
2016-04-03 15:02:23 -0700

    Create leetcode_longest-substring-with-at-most-k-distinct-characters.cpp

---
+++ LeetCode/leetcode_longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,60 @@
+/*
+*
+* Tag: Hash
+* Time: O(n)
+* Space: O(1)
+*/
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int ans = 0;
+        if(!s.size() || !k)
+            return ans;
+        int cntk = 0, j = 0, n = s.size();
+        for(int i = 0; i < n; ++ i){
+            if(dict[s[i]] == 0){
+                ++ cntk;
+            }
+            ++ dict[s[i]];
+            while(cntk > k && j <= i){
+                -- dict[s[j]];
+                if(dict[s[j]] == 0)
+                    -- cntk;
+                ++ j;
+            }
+            ans = max(ans, i - j + 1);
+        }
+        return ans;
+    }
+private:
+unordered_map<char,int> dict;
+};
+
+/*
+* A more efficient solution
+*/
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int ans = 0;
+        if(!s.size() || !k)
+            return ans;
+        int cntk = 0, j = 0, n = s.size();
+        for(int i = 0; i < n; ++ i){
+            if(dict[(int)s[i]] == 0){
+                ++ cntk;
+            }
+            ++ dict[(int)s[i]];
+            while(cntk > k && j <= i){
+                -- dict[(int)s[j]];
+                if(dict[(int)s[j]] == 0)
+                    -- cntk;
+                ++ j;
+            }
+            ans = max(ans, i - j + 1);
+        }
+        return ans;
+    }
+private:
+int dict[300];
+};
