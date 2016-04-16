3d1bdf6d400a86817d0fe271633cb542bb83cbff
Baichuan Yang <1062969706@qq.com>
2016-04-16 12:06:39 -0700

    Create coding_interview_template.cpp

---
+++ coding_interview_template.cpp
@@ -0,0 +1,71 @@
+/*
+*
+* Author: BaiChuan Yang
+* Date: MM/DD/YYYY
+* Language: C++
+*/
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<cmath>
+#include<climits>
+#include<iostream>
+#include<bitset>
+#include<string>
+#include<list>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+#include<utility>
+using namespace std;
+
+/*
+*
+* Tag: Algorithm Category
+* Time: O()
+* Space: O()
+* Input:
+* Output:
+*/
+void function_name(){
+    return ;
+}
+/*
+* Test
+1.Normal
+Input:
+
+Output:
+
+2.Edge (Min)
+Input:
+
+Output:
+
+3.Edge (Max)
+Input:
+
+Output:
+
+4.Wrong Input
+Input:
+
+Output:
+
+5.Normal (Complex)
+Input:
+
+Output:
+
+...
+*/
+
+int main(){
+    return 0;
+}
