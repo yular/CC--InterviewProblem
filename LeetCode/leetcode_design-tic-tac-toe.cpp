43519f2626909190e7c23c0fb73af14ece887cb4
Baichuan Yang <1062969706@qq.com>
2016-05-28 06:49:41 -0700

    Update leetcode_design-tic-tac-toe.cpp

--- LeetCode/leetcode_design-tic-tac-toe.cpp
+++ LeetCode/leetcode_design-tic-tac-toe.cpp
@@ -18,14 +18,7 @@ public:
         bnd = n;
     }
     
-    /** Player {player} makes a move at ({row}, {col}).
-        @param row The row of the board.
-        @param col The column of the board.
-        @param player The player, can be either 1 or 2.
-        @return The current winning condition, can be either:
-                0: No one wins.
-                1: Player 1 wins.
-                2: Player 2 wins. */
+
     int move(int row, int col, int player) {
         int res = 0;
         board[row][col] = player;
@@ -60,8 +53,35 @@ int bnd;
 int dir[8][2] = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
 };
 
-/**
- * Your TicTacToe object will be instantiated and called as such:
- * TicTacToe obj = new TicTacToe(n);
- * int param_1 = obj.move(row,col,player);
- */
+/*
+*
+* Tag: Implementation
+* Time: O(1)
+* Space: O(n)
+*/
+class TicTacToe {
+public:
+    /** Initialize your data structure here. */
+    TicTacToe(int n) {
+        rows.resize(n);
+        cols.resize(n);
+        diag = antidiag = 0;
+        bnd = n;
+    }
+    
+    int move(int row, int col, int player) {
+        int res = 0;
+        int add = player == 1?1:-1;
+        rows[row] += add;
+        cols[col] += add;
+        diag += row == col?add:0;
+        antidiag += row == bnd - 1 - col?add:0;
+        if(abs(rows[row]) == bnd || abs(cols[col]) == bnd || abs(diag) == bnd || abs(antidiag) == bnd )
+            res = player == 1?1:2;
+        return res;
+    }
+private:
+vector<int> rows, cols;
+int bnd, diag, antidiag;
+};
+
