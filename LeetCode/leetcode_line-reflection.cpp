9163e72b481edcbe9c64355936a958364ccabf3d
Baichuan Yang <1062969706@qq.com>
2016-06-13 20:06:13 -0700

    Update leetcode_line-reflection.cpp

--- LeetCode/leetcode_line-reflection.cpp
+++ LeetCode/leetcode_line-reflection.cpp
@@ -27,3 +27,25 @@ public:
 private:
    set<pair<int, int>> xy_axis;  
 };
+
+class Solution {
+public:
+    bool isReflected(vector<pair<int, int>>& points) {
+        if((int)points.size() <= 1)
+            return true;
+        int y = 0, minx = INT_MAX, maxx = INT_MIN;
+        for(int i = 0; i < points.size(); ++ i){
+            minx = min(minx, points[i].first);
+            maxx = max(maxx, points[i].first);
+            xy_axis[points[i].second].insert(points[i].first);
+        }
+        y = maxx + minx;
+        for(int i = 0; i < points.size(); ++ i){
+            if(xy_axis[points[i].second].find(y - points[i].first) == xy_axis[points[i].first].end())
+                return false;
+        }
+        return true;
+    }
+private:
+   unordered_map<int, unordered_set<int>> xy_axis;  
+};
