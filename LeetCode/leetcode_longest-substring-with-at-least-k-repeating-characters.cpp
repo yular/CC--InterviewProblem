9ac1a9644551b335f5f563e90b201344c480ff93
Baichuan Yang <1062969706@qq.com>
2016-09-06 00:04:21 -0700

    Update leetcode_longest-substring-with-at-least-k-repeating-characters.cpp

--- LeetCode/leetcode_longest-substring-with-at-least-k-repeating-characters.cpp
+++ LeetCode/leetcode_longest-substring-with-at-least-k-repeating-characters.cpp
@@ -7,27 +7,32 @@
 class Solution {
 public:
     int longestSubstring(string s, int k) {
-        int res = 0, n = s.size(), right_char_cnt = 0, fidx = 0;
+        int res = 0, n = s.size(), right_char_cnt = 0, fidx = 0, bidx = 0;
         if(!s.size() || k <= 1)
             return n;
-        vector<int> cnt(26, 0), totcnt(26, 0);
+        vector<int> cnt(26, 0), totcnt(26, 0), tmpcnt(26, 0);
         for(int i = 0; i < n; ++ i)
             ++ totcnt[s[i] - 'a'];
         for(int i = 0; i < n; ++ i){
             if(totcnt[s[i] - 'a'] < k){
-                while(fidx < i){
-                    -- cnt[s[fidx] - 'a'];
-                    if(cnt[s[fidx] - 'a'] == 0)
-                        -- right_char_cnt;
-                    if(check(cnt, k))
-                        res = max(res, i - fidx - 1);
-                    ++ fidx;
-                }
-                ++ fidx;
-                right_char_cnt = 0;
-                
                 for(int j = 0; j < 26; ++ j)
                     totcnt[j] -= cnt[j];
+                bidx = i - 1;
+                while(cnt[s[fidx] - 'a'] < k && fidx < i){
+                    -- cnt[s[fidx] - 'a'];
+                    ++ fidx;
+                }
+                while(cnt[s[bidx] - 'a'] < k && fidx < bidx){
+                    -- cnt[s[bidx] - 'a'];
+                    -- bidx;
+                }
+                
+                if(fidx < bidx && check(cnt, k))
+                    res = max(res, bidx - fidx + 1);
+                for(int j = 0; j < 26; ++ j)
+                    cnt[j] = 0;
+                fidx = i + 1;
+                right_char_cnt = 0;
                 
             }else{
                 ++ cnt[s[i] - 'a'];
@@ -38,6 +43,7 @@ public:
                 if(!right_char_cnt)
                     res = max(res, i + 1 - fidx);
             }
+            printf("%d -- %d\n",i,res);
         }
         return res;
     }
