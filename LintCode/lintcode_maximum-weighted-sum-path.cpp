193e9e795fe3b37eddbacc4f751e8a26942bf38e
Baichuan Yang <1062969706@qq.com>
2018-03-04 00:16:13 -0800

    Update lintcode_maximum-weighted-sum-path.cpp

--- LintCode/lintcode_maximum-weighted-sum-path.cpp
+++ LintCode/lintcode_maximum-weighted-sum-path.cpp
@@ -2,26 +2,28 @@
 *
 * Tag: DP
 * Time: O(nm)
-* Space: O(nm) (In optimal solution, it is O(m))
+* Space: O(m)
 */
 class Solution {
 public:
     int maxWeight(vector<vector<int>> &nums) {
         int n = nums.size(), m = nums[0].size();
-        vector<vector<int>> dp(nums);
+        vector<int> dp(m, 0), predp(m, 0);
         
+        dp[m - 1] = nums[0][m - 1];
         for(int i = 0; i < n; ++ i){
             for(int j = m - 1; j >= 0; -- j){
-                int maxSum = dp[i][j];
+                int maxSum = dp[j];
                 if(isInGrid(i, j + 1, n, m))
-                    maxSum = max(maxSum, nums[i][j] + dp[i][j + 1]);
+                    maxSum = max(maxSum, nums[i][j] + dp[j + 1]);
                 if(isInGrid(i - 1, j, n, m))
-                    maxSum = max(maxSum, nums[i][j] + dp[i - 1][j]);
-                dp[i][j] = maxSum;
+                    maxSum = max(maxSum, nums[i][j] + predp[j]);
+                dp[j] = maxSum;
             }
+            predp = dp;
         }
         
-        return dp[n - 1][0];
+        return dp[0];
     }
     
 private:
