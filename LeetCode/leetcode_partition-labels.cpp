1505f0aff537793f1c56fff378e380b29ffe3244
Baichuan Yang <1062969706@qq.com>
2018-06-17 15:47:25 -0700

    Update leetcode_partition-labels.cpp

--- LeetCode/leetcode_partition-labels.cpp
+++ LeetCode/leetcode_partition-labels.cpp
@@ -18,17 +18,14 @@ public:
             rightBoundOfChar[indexOfChar] = i;
         }
         
-        int curPartitionRightBound = rightBoundOfChar[(int)(S[0] - 'a')];
+        int curPartitionRightBound = 0, anchor = 0;
         for(int i = 0; i < S.size(); ++ i){
             int indexOfChar = S[i] - 'a';
             curPartitionRightBound = max(curPartitionRightBound, rightBoundOfChar[indexOfChar]);
             if(i == curPartitionRightBound) {
-                ans.push_back( i + 1 );
+                ans.push_back( i - anchor + 1 );
+                anchor = i + 1;
             }
-        }
-        
-        for(int i = ans.size() - 1; i > 0; -- i) {
-            ans[i] = ans[i] - ans[i - 1];
         }
         
         return ans;
