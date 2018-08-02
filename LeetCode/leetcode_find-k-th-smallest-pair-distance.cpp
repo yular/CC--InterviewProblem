4319f65e8c60d627a046d4f9466e5272c7081a9a
Baichuan Yang <1062969706@qq.com>
2018-08-02 02:26:38 -0700

    Create leetcode_find-k-th-smallest-pair-distance.cpp

---
+++ LeetCode/leetcode_find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,34 @@
+/*
+*
+* Tag: Binary Search
+* Time: O()
+* Space: O(1)
+*/
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        int l = 0, r = 1000000;
+        while(l < r){
+            int mid = (r + l)>>1;
+            int cnt = 0;
+            for(int i = 0, j = 0; i < n; ++ i){
+                while(j < n && nums[j] - nums[i] <= mid) {
+                    ++ j;
+                }
+                
+                cnt += j - i - 1;
+            }
+            
+            if(cnt < k){
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        
+        return l;
+    }
+};
