29ac601200c82216fe1156180628acf189e1cc98
Baichuan Yang <1062969706@qq.com>
2018-08-13 21:46:23 -0700

    Update leetcode_counting-bits.cpp

--- LeetCode/leetcode_counting-bits.cpp
+++ LeetCode/leetcode_counting-bits.cpp
@@ -4,16 +4,27 @@
 * Time: O(n)
 * Space: O(n)
 */
+/*
+*
+* TODO
+* Tag: DP
+* Time: O(n)
+* Space: O(n)
+*/
 class Solution {
 public:
     vector<int> countBits(int num) {
         vector<int> ans(num + 1);
         ans[0] = 0;
-        if(!num)
+        if(!num) {
             return ans;
+        }
+        
         ans[1] = 1;
-        if(num == 1)
+        if(num == 1) {
             return ans;
+        }
+        
         ans[2] = 1;
         int lowbnd = 2, upbnd = 4;
         for(int i = 3; i <= num; ++ i){
@@ -26,6 +37,7 @@ public:
                 ans[i] = ans[mask] + 1;
             }
         }
+        
         return ans;
     }
 };
