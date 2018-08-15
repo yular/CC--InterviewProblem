a5ded0917d3df2bf14a926e74c43083681b987b2
Baichuan Yang <1062969706@qq.com>
2018-08-15 00:58:15 -0700

    Update leetcode_my-calendar-iii.cpp

--- LeetCode/leetcode_my-calendar-iii.cpp
+++ LeetCode/leetcode_my-calendar-iii.cpp
@@ -6,22 +6,22 @@
 */
 class MyCalendarThree {
 private:
-    map<int, int> timeScheduler;
+    map<int,int> timeScheduler;
 public:
     MyCalendarThree() {
         timeScheduler.clear();
     }
     
     int book(int start, int end) {
-        timeScheduler[start] = timeScheduler.count(start) == 0? 1 : timeScheduler[start] + 1;
-        timeScheduler[end] = timeScheduler.count(end) == 0? -1 : timeScheduler[end] - 1;
+        ++ timeScheduler[start];
+        -- timeScheduler[end];
         
-        int delta = 0, ans = 0;
+        int ans = 0, delta = 0;
         for(auto it = timeScheduler.begin(); it != timeScheduler.end(); ++ it){
             delta += it->second;
             ans = max(ans, delta);
         }
-                
+        
         return ans;
     }
 };
