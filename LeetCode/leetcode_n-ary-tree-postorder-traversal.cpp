b18be5537c7a21ac93c7013a4bf359d297d4a510
Baichuan Yang <1062969706@qq.com>
2018-08-18 16:28:39 -0700

    Update leetcode_n-ary-tree-postorder-traversal.cpp

--- LeetCode/leetcode_n-ary-tree-postorder-traversal.cpp
+++ LeetCode/leetcode_n-ary-tree-postorder-traversal.cpp
@@ -19,15 +19,27 @@ public:
     }
 };
 */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
 class Solution {
 public:
     vector<int> postorder(Node* root) {
         vector<int> ans;
-        if(root == NULL){
-            return ans;
-        }
-        
         dfs(root, ans);
+        
         return ans;
     }
     
