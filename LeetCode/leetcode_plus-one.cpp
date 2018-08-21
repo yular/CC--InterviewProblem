1d9f2eab3b462165c30a8bea64f134b8cf6198fd
Baichuan Yang <1062969706@qq.com>
2018-08-21 15:25:46 -0700

    Update leetcode_plus-one.cpp

--- LeetCode/leetcode_plus-one.cpp
+++ LeetCode/leetcode_plus-one.cpp
@@ -7,21 +7,24 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
-        vector<int> ans;
+        list<int> newDigits;
         int c = 0, add = 1, sum = 0;
         for(int i = digits.size() - 1; i >= 0; i --){
             sum = digits[i] + c + add;
             add = 0;
-            if(sum >= 10)
+            if(sum >= 10){
                 c = 1;
-            else
+            } else {
                 c = 0;
+            }
             sum %= 10;
-            digits[i] = sum;
+            newDigits.push_front(sum);
         }
-        if(c)
-            digits.insert(digits.begin(), 1);
-        ans = digits;
+        if(c){
+            newDigits.push_front(1);
+        }
+        
+        vector<int> ans(newDigits.begin(), newDigits.end());
         return ans;
     }
 };
