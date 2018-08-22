9f760aafb5c04d65767b80d1f48b6cc436f07bc7
Baichuan Yang <1062969706@qq.com>
2018-08-21 19:44:20 -0700

    Update leetcode_power-of-two.cpp

--- LeetCode/leetcode_power-of-two.cpp
+++ LeetCode/leetcode_power-of-two.cpp
@@ -1,35 +1,12 @@
 /*
 *
 * Tag: Math
-* Time: O(lgn)
+* Time: O(1)
 * Space: O(1)
 */
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n <= 0? false : log10(n)/log10(2) == floor(log10(n)/log10(2));
-    }
-};
-
-/*
-*
-* Tag: Math & Bit Manipulation
-* Time: O(lgn)
-* Space: O(1)
-*/
-class Solution {
-public:
-    bool isPowerOfTwo(int n) {
-        if(n <= 0)
-            return false;
-        int cnt_one = 0;
-        while(n){
-            if(n&1)
-                ++ cnt_one;
-            if(cnt_one > 1)
-                return false;
-            n>>=1;
-        }
-        return true;
+        return n>0 && (n&(n-1))==0;
     }
 };
