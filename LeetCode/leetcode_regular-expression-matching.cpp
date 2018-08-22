6677e89c2bc547f8f1a7aed26e0778d658dab494
Baichuan Yang <1062969706@qq.com>
2018-08-21 20:42:50 -0700

    Update leetcode_regular-expression-matching.cpp

--- LeetCode/leetcode_regular-expression-matching.cpp
+++ LeetCode/leetcode_regular-expression-matching.cpp
@@ -7,7 +7,7 @@
 //dp[i][j] = dp[0][0] = true
 //           dp[i - 1][j - 1] where p[i] == s[j] || p[i] == '.'
 //           dp[i - 2][j]  where p[i] == '*' && p[i - 1] != s[j]
-//           dp[i - 2][j] (remove * and p[i - 1]) || dp[i][j - 1] (repeat p[i - 1] >= 1 time(s)) where p[i] == '*' && (p[i - 1] == '.' || p[i - 1] == s[j])
+//           dp[i - 2][j] (remove * ) || dp[i - 1][j] (retain p[i - 1]) || dp[i][j - 1] (repeat p[i - 1] >= 1 time(s)) where p[i] == '*' && (p[i - 1] == '.' || p[i - 1] == s[j])
 //dp[lenP][lenS]
 class Solution {
 public:
@@ -29,7 +29,7 @@ public:
                     dp[i][j] = dp[i - 1][j - 1];
                 } else if(p[i - 1] == '*'){
                     if(p[i - 2] == '.' || p[i - 2] == s[j - 1]) {
-                        dp[i][j] = dp[i - 2][j] || dp[i][j - 1];
+                        dp[i][j] = dp[i - 2][j] || dp[i - 1][j] || dp[i][j - 1];
                     } else {
                         dp[i][j] = dp[i - 2][j];
                     }
