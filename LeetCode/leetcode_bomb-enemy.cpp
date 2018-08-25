29ba441c653d2218d0b508df41c1bd86b9282842
Baichuan Yang <1062969706@qq.com>
2018-08-25 13:26:24 -0700

    Update leetcode_bomb-enemy.cpp

--- LeetCode/leetcode_bomb-enemy.cpp
+++ LeetCode/leetcode_bomb-enemy.cpp
@@ -8,10 +8,11 @@ class Solution {
 public:
     int maxKilledEnemies(vector<vector<char>>& grid) {
         int ans = 0;
-        if(!grid.size())
+        if(!grid.size()){
             return ans;
-        int dp[grid.size() + 2][grid[0].size() + 2][4];
+        }
         
+        int dp[grid.size() + 2][grid[0].size() + 2][4];     
         memset(dp, 0, sizeof(dp));
         
         for(int i = 0; i < grid.size(); ++ i){
@@ -44,8 +45,9 @@ public:
         
         for(int i = 0; i < grid.size(); ++ i){
             for(int j = 0; j < grid[0].size(); ++ j){
-                if(grid[i][j] == '0')
+                if(grid[i][j] == '0'){
                     ans = max(ans, dp[i + 1][j + 1][0] + dp[i + 1][j + 1][1] + dp[i + 1][j + 1][2] + dp[i + 1][j + 1][3]);
+                }
             }
         }
         
