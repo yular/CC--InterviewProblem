542b029717389f0adfba56a4f227f69f085f254a
Baichuan Yang <1062969706@qq.com>
2018-08-24 17:41:50 -0700

    Update leetcode_largest-rectangle-in-histogram.cpp

--- LeetCode/leetcode_largest-rectangle-in-histogram.cpp
+++ LeetCode/leetcode_largest-rectangle-in-histogram.cpp
@@ -6,19 +6,24 @@
 */
 class Solution {
 public:
-    int largestRectangleArea(vector<int> &height) {
-        int res = 0, idx = 0;
+    int largestRectangleArea(vector<int>& heights) {
+        int ans = 0;
+        if(heights.size() == 0){
+            return ans;
+        }
+        
         stack<int> stk;
-        height.push_back(0);
-        for(int i = 0; i < height.size();){
-            if(stk.empty() || height[stk.top()] < height[i])
+        heights.push_back(0);
+        for(int i = 0; i < heights.size();) {
+            if(stk.empty() || heights[stk.top()] < heights[i]) {
                 stk.push(i ++);
-            else{
-                idx = stk.top();
+            } else {
+                int idx = stk.top();
                 stk.pop();
-                res = max(res, height[idx]*(stk.empty()?i:(i - stk.top() - 1)));
+                ans = max(ans, heights[idx]*(stk.empty() ? i : (i - stk.top() - 1)));
             }
         }
-        return res;
+        
+        return ans;
     }
 };
