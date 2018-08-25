7e7ac26e5ed6448c8c3e31b176153e6b0dc4196c
Baichuan Yang <1062969706@qq.com>
2018-08-25 00:39:42 -0700

    Update leetcode_longest-substring-with-at-most-k-distinct-characters.cpp

--- LeetCode/leetcode_longest-substring-with-at-most-k-distinct-characters.cpp
+++ LeetCode/leetcode_longest-substring-with-at-most-k-distinct-characters.cpp
@@ -2,7 +2,7 @@
 *
 * Tag: Hash
 * Time: O(n)
-* Space: O(1)
+* Space: O(k)
 */
 class Solution {
 public:
@@ -34,27 +34,34 @@ unordered_map<char,int> dict;
 * A more efficient solution
 */
 class Solution {
+private:
+    int dict[300];
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
         int ans = 0;
-        if(!s.size() || !k)
+        if(!s.size() || !k){
             return ans;
+        }
+        
         int cntk = 0, j = 0, n = s.size();
         for(int i = 0; i < n; ++ i){
             if(dict[(int)s[i]] == 0){
                 ++ cntk;
             }
+            
             ++ dict[(int)s[i]];
             while(cntk > k && j <= i){
                 -- dict[(int)s[j]];
-                if(dict[(int)s[j]] == 0)
+                if(dict[(int)s[j]] == 0){
                     -- cntk;
+                }
+                
                 ++ j;
             }
+            
             ans = max(ans, i - j + 1);
         }
+        
         return ans;
     }
-private:
-int dict[300];
 };
