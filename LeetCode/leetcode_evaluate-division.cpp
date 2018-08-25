181d3c766d540433ada8ab96cd0693122116f7cd
Baichuan Yang <1062969706@qq.com>
2018-08-25 16:55:50 -0700

    Update leetcode_evaluate-division.cpp

--- LeetCode/leetcode_evaluate-division.cpp
+++ LeetCode/leetcode_evaluate-division.cpp
@@ -8,8 +8,10 @@ class Solution {
 public:
     vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> query) {
         vector<double> ans(query.size());
-        if(!query.size())
+        if(!query.size()) {
             return ans;
+        }
+        
         unordered_map<string, unordered_map<string,double>> mp;
         unordered_set<string> vis;
         mp.clear();
@@ -35,6 +37,7 @@ public:
         
         return ans;
     }
+    
 private:
     void dfs(string &sta, string &ends, double &res, double &tmpres, bool &canReach, unordered_map<string, unordered_map<string,double>> &mp, unordered_set<string> &vis){
         if(sta == ends){
@@ -42,16 +45,25 @@ private:
             canReach = true;
             return ;
         }
+        
         unordered_map<string,double>::iterator it = mp[sta].begin();
         double vres = 1.0;
         string u = "";
         for(; it != mp[sta].end(); ++ it){
-            if(vis.find(it->first) != vis.end())
+            if(vis.find(it->first) != vis.end()){
                 continue;
+            }
+            
             vres = tmpres*it->second;
             u = it->first;
             vis.insert(it->first);
             dfs(u, ends, res, vres, canReach, mp, vis);
+            
+            if(canReach) {
+                break ;
+            }
         }
+        
+        return ;
     }
 };
