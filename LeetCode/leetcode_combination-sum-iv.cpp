80c5d06769c8c19e739ffe50fa119d82a4a3a64e
Baichuan Yang <1062969706@qq.com>
2018-08-25 13:53:11 -0700

    Update leetcode_combination-sum-iv.cpp

--- LeetCode/leetcode_combination-sum-iv.cpp
+++ LeetCode/leetcode_combination-sum-iv.cpp
@@ -7,17 +7,20 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target + 1);
-        if(!nums.size() || !target)
+        vector<int> dp(target + 1, 0);
+        if(!nums.size() || !target) {
             return 0;
-        int n = nums.size();
+        }
+        
         sort(nums.begin(), nums.end());
+        
         dp[0] = 1;
         for(int i = 1; i <= target; ++ i){
             for(int j = 0; j < nums.size() && nums[j] <= i; ++ j){
                 dp[i] += dp[i - nums[j]];
             }
         }
+        
         return dp[target];
     }
 };
