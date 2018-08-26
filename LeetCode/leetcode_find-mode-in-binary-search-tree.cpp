a554a74b09c5bf809d5643d5770c7e2eaf3c940f
Baichuan Yang <1062969706@qq.com>
2018-08-25 17:30:57 -0700

    Update leetcode_find-mode-in-binary-search-tree.cpp

--- LeetCode/leetcode_find-mode-in-binary-search-tree.cpp
+++ LeetCode/leetcode_find-mode-in-binary-search-tree.cpp
@@ -2,31 +2,50 @@
 *
 * Tag: DFS
 * Time: O(n)
-* Space: O(1)
+* Space: O(n)
 */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> findMode(TreeNode* root) {
         vector<int> ans;
-        if(root == NULL)
+        if(root == NULL) {
             return ans;
+        }
+        
         TreeNode* p = root;
-        while(p->right) p = p->right;
-        if(p->val < INT_MAX)
+        while(p->right) { 
+            p = p->right;
+        }
+        
+        if(p->val < INT_MAX) {
             p->right = new TreeNode(p->val + 1);
-        else
+        } else {
             p->right = new TreeNode(p->val - 1);
+        }
+        
         long long cmpval = INT_MIN - 1;
         int cnt = 0, maxcnt = -1;
         dfs(root, cnt, maxcnt, cmpval, ans);
         p->right = NULL;
+        
         return ans;
     }
     
 private:
     void dfs(TreeNode *root, int &cnt, int &maxcnt, long long &cmpval, vector<int> &ans){
-        if(root->left)
+        if(root->left) {
             dfs(root->left, cnt, maxcnt, cmpval, ans);
+        }
+        
         if(root->val != cmpval){
             if(cnt >= maxcnt){
                 if(cnt > maxcnt)
@@ -40,7 +59,9 @@ private:
         }else{
             ++ cnt;
         }
-        if(root->right)
+        
+        if(root->right) {
             dfs(root->right, cnt, maxcnt, cmpval, ans);
+        }
     }
 };
