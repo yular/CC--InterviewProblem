bebd6bf426af2f40fb20a24874a133703b350894
Baichuan Yang <1062969706@qq.com>
2018-08-25 22:12:31 -0700

    Update leetcode_lonely-pixel-i.cpp

--- LeetCode/leetcode_lonely-pixel-i.cpp
+++ LeetCode/leetcode_lonely-pixel-i.cpp
@@ -8,8 +8,10 @@ class Solution {
 public:
     int findLonelyPixel(vector<vector<char>>& picture) {
         int ans = 0;
-        if(picture.size() == 0)
+        if(picture.size() == 0){
             return ans;
+        }
+        
         int n = picture.size(), m = picture[0].size();
         vector<int> rb_cnt(n, 0), cb_cnt(m, 0);
         for(int i = 0; i < n; ++ i){
@@ -20,6 +22,7 @@ public:
                 }
             }
         }
+        
         for(int i = 0; i < n; ++ i){
             for(int j = 0; j < m; ++ j){
                 if(picture[i][j] == 'B' && rb_cnt[i] == 1 && cb_cnt[j] == 1){
@@ -27,6 +30,7 @@ public:
                 }
             }
         }
+        
         return ans;
     }
 };
