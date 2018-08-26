c4d021319b67532ff579d8180de8cc66a3d6e401
Baichuan Yang <1062969706@qq.com>
2018-08-25 23:54:25 -0700

    Update leetcode_minimum-absolute-difference-in-bst.cpp

--- LeetCode/leetcode_minimum-absolute-difference-in-bst.cpp
+++ LeetCode/leetcode_minimum-absolute-difference-in-bst.cpp
@@ -18,17 +18,22 @@ public:
     int getMinimumDifference(TreeNode* root) {
         int last_v = -1, cur_v, ans = INT_MAX;
         dfs(root, last_v, ans);
+        
         return ans;
     }
 private:
     void dfs(TreeNode *root, int &last_v, int &ans){
-        if(root->left)
+        if(root->left) {
             dfs(root->left, last_v, ans);
+        }
+        
         if(last_v >= 0){
             ans = min(ans, root->val - last_v);
         }
         last_v = root->val;
-        if(root->right)
+        
+        if(root->right) {
             dfs(root->right, last_v, ans);
+        }
     }
 };
