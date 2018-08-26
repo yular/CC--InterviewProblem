270a075187ed2984af8126d5301972c4f81f9688
Baichuan Yang <1062969706@qq.com>
2018-08-25 17:25:07 -0700

    Update leetcode_find-k-pairs-with-smallest-sums.cpp

--- LeetCode/leetcode_find-k-pairs-with-smallest-sums.cpp
+++ LeetCode/leetcode_find-k-pairs-with-smallest-sums.cpp
@@ -16,22 +16,28 @@ struct node{
 public:
     vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<pair<int,int>> ans;
-        if(!nums1.size() || !nums2.size() || !k)
+        if(!nums1.size() || !nums2.size() || !k) {
             return ans;
+        }
+        
         priority_queue<node> pq;
         node a;
         for(int i = 0; i < min(nums1.size(), nums2.size()); ++ i){
             a.fval = i, a.sval = i, a.sum = nums1[i]+nums2[i];
             pq.push(a);
         }
+        
         int id = 0;
         while(id < k && !pq.empty()){
             node cur = pq.top(), nxt;
             pq.pop();
+            
             ans.push_back(make_pair(nums1[cur.fval], nums2[cur.sval]));
             ++ id;
-            if(id >= k)
+            if(id >= k) {
                 break;
+            }
+            
             if(cur.sval+1 != cur.fval && cur.sval + 1 > cur.fval && cur.sval+1 < nums2.size()){
                 nxt.fval = cur.fval, nxt.sval = cur.sval + 1, nxt.sum = nums1[cur.fval] + nums2[cur.sval + 1];
                 pq.push(nxt);
@@ -41,6 +47,7 @@ public:
                 pq.push(nxt);
             }
         }
+        
         return ans;
     }
 };
