acbc598e4f403f6ce554781537b5b541faf465b4
Baichuan Yang <1062969706@qq.com>
2020-04-19 12:14:03 +0800

    Create leetcode_find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp

---
+++ LeetCode/leetcode_find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -0,0 +1,31 @@
+/*
+*
+* Tag: Greedy
+* Time: O()
+* Space: O()
+*/
+
+class Solution {
+public:
+    int findMinFibonacciNumbers(int k) {
+        set<int> st = {1, 1};
+        int a = 1, b = 1, sum = 0;
+        for(; sum < k; ) {
+            sum = a + b;
+            st.insert(sum);
+            a = b, b = sum;
+        }
+
+        int cnt = 0;
+        while(k > 0) {
+            auto it = st.lower_bound(k);
+            ++ cnt;
+            if(*it == k) {
+                break;
+            }
+            it --;
+            k -= *it;
+        }
+        return cnt;
+    }
+};
