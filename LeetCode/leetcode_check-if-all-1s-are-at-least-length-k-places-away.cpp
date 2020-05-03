59effc2bf148e7ba6bb397c31bacf62799b5a783
Baichuan Yang <1062969706@qq.com>
2020-05-03 16:10:06 +0800

    Create leetcode_check-if-all-1s-are-at-least-length-k-places-away.cpp

---
+++ LeetCode/leetcode_check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,24 @@
+/*
+*
+* Tag: Sliding Window
+* Time: O(n)
+* Space: O(1)
+*/
+
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int prev = -k-1;
+        for(int i = 0; i < nums.size(); ++ i) {
+            if(nums[i] == 0) {
+                continue;
+            }
+            
+            if(i - prev <= k) {
+                return false;
+            }
+            prev = i;
+        }
+        return true;
+    }
+};
