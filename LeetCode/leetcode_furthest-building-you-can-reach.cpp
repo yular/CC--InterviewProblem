f9f9216d77ceb381a0aed288c81a5ab392e25b2e
Baichuan Yang <1062969706@qq.com>
2020-11-01 16:35:00 +0800

    Create leetcode_furthest-building-you-can-reach.cpp

---
+++ LeetCode/leetcode_furthest-building-you-can-reach.cpp
@@ -0,0 +1,32 @@
+/*
+*
+* Tag: Greedy
+* Time: O(nlgn)
+* Space: O(n)
+*/
+
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        if(heights.size() == 0) {
+            return 0;
+        }
+        
+        int ans = 0, n = heights.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i = 0; i < n - 1; ++ i) {
+            int d = heights[i + 1] - heights[i];
+            if(d > 0) {
+                pq.push(d);
+            }
+            if(pq.size() > ladders) {
+                bricks -= pq.top();
+                pq.pop();
+            }
+            if(bricks < 0) {
+                return i;
+            }
+        }
+        return n - 1;
+    }
+};
