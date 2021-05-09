e0328337f691934ae7fd562900d981b4cd1941ad
Baichuan Yang <1062969706@qq.com>
2021-05-09 14:51:54 +0800

    Create leetcode_maximum-distance-between-a-pair-of-values.cpp

---
+++ LeetCode/leetcode_maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,32 @@
+/*
+*
+* Tag: Hash
+* Time: O(nlgn)
+* Space: O(n)
+*/
+
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() == 0 || nums2.size() == 0) {
+            return 0;
+        }
+        
+        int ans = 0, n = nums1.size(), m = nums2.size();
+        map<int,int> numDict;
+        for(int i = n - 1, j = m - 1; i >= 0; -- i) {
+            while(j >= i) {
+                numDict[nums2[j]] = max(numDict[nums2[j]], j);
+                -- j;
+            }
+            
+            auto it = numDict.lower_bound(nums1[i]);
+            if(it == numDict.end()) {
+                continue;
+            }
+            int dis = abs(it->second - i);
+            ans = max(ans, dis);
+        }
+        return ans;
+    }
+};
